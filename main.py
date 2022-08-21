3c3a51890492ad5f0bdfbbe076be8bd465b5c780
Bruno M-deMaria <brunomartinsdemaria@gmail.com>
2022-08-21 09:18:33 -0300

    corrigido indice do loop

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from data import question_data
 
 question_bank = []                      #Criando um banco de perguntas com objetos 
 for question in question_data:
-    question_text = question_data["text"]
-    question_answer = question_data["answer"]
+    question_text = question["text"]
+    question_answer = question["answer"]
     new_question = Question(question_text,question_answer)
     question_bank.append(new_question)
 
-print(question_bank)
+print(question_bank[0].answer)
+
+
 
