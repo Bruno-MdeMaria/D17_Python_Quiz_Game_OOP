dd4ad98ff209d203578092af4056161079745234
Bruno M-deMaria <brunomartinsdemaria@gmail.com>
2022-08-21 07:37:24 -0300

    Criando Classes
    Criando Classes

---
+++ main.py
@@ -0,0 +1,16 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+class User:
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+
+usuario_1 = User("001", "Bruno")
+print(usuario_1)
+
+
+
+
