d648a52f47f04fdddb2aa0a29add70fb66ea62cf
Bruno M-deMaria <brunomartinsdemaria@gmail.com>
2022-08-22 00:41:31 -0300

    Update quiz_brain.py
    Adicionado class Quiz Brasin para montar a questão.

---
+++ quiz_brain.py
@@ -0,0 +1,10 @@
+class QuizBrain:
+    def __init__(self, q_list):                     
+        self.question_nunber = 0
+        self.question_list = q_list         #Lista de questões será o nosso banco de questoes.
+                                            #Ex. QuizBrain(question_bank)
+
+
+
+
+
